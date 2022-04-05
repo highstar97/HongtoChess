7d5dd8f5220b5ee70768733beb06b71c94fd7c1e
highstar97 <pgt0185@gmail.com>
2022-04-05 22:17:51 +0900

    BaseChampion Class 생성

---
+++ Source/HongtoChess/Champion/BaseChampion.cpp
@@ -0,0 +1,19 @@
+#include "Champion/BaseChampion.h"
+//#include "Engine/Texture2D.h"
+
+ABaseChampion::ABaseChampion()
+{
+
+}
+
+void ABaseChampion::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABaseChampion::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
---
+++ Source/HongtoChess/Champion/BaseChampion.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "BaseChampion.generated.h"
+
+class UTexture2D;
+
+UENUM()
+enum class ECountry : uint8
+{
+	WE,
+	CHOCK,
+	OH,
+	HAN
+};
+
+UENUM()
+enum class ESynergy : uint8
+{
+	DOWONGYEOLUI,
+};
+
+UCLASS()
+class HONGTOCHESS_API ABaseChampion : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABaseChampion();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, Category = "Champion")
+	int32 SerialNumber;
+
+	UPROPERTY(EditInstanceOnly, Category = "Champion")
+	int32 PlayerNumber;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Champion")
+	FString Name;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Champion")
+	int32 Cost;
+
+	UPROPERTY(EditAnywhere, Category = "Champion")
+	int32 Star;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Champion")
+	ECountry Country;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Champion")
+	TArray<ESynergy> Synergy;
+
+	//UPROPERTY(EditAnywhere, Category = "Stat")
+	//UStat Stat;
+	
+	//UPROPERTY(VisibleDefaultsOnly, Category = "Skill")
+	//USkill Skill;
+
+	//UPROPERTY(EditAnywhere, Category = "Item")
+	//TArray<UItem*> Item;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Champion")
+	UTexture2D* Picture;
+};
