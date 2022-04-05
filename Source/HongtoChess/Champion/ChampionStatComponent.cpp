7b91da4bb3ec68488d2082da31bfcd4662957266
highstar97 <pgt0185@gmail.com>
2022-04-06 00:43:20 +0900

    ChampionStatComponent 생성

---
+++ Source/HongtoChess/Champion/ChampionStatComponent.cpp
@@ -0,0 +1,38 @@
+#include "Champion/ChampionStatComponent.h"
+
+UChampionStatComponent::UChampionStatComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+	CurrentHP = 0;
+	MaxHP = 0;
+	CurrentMP = 0;
+	MaxMP = 0;
+	AttackDamage = 0;
+	AbilityPower = 0;
+	Defense = 0;
+	MagicResistance = 0;
+	AttackSpeed = 0.0f;
+	Range = 0;
+	CriticalRate = 0;
+	CriticalCoefficient = 0.0f;
+}
+
+void UChampionStatComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UChampionStatComponent::UpgradeStat()
+{
+	MaxHP *= 1.8;
+	CurrentHP = MaxHP;
+
+	AttackDamage *= 1.8;
+}
+
+void UChampionStatComponent::InitStatData(int SerialNumber)
+{
+	// 엑셀 데이터에서 SerialNumber에 맞는 데이터를 불러와서 1성 Stat에 대입
+	
+}
---
+++ Source/HongtoChess/Champion/ChampionStatComponent.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "ChampionStatComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class HONGTOCHESS_API UChampionStatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UChampionStatComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	void UpgradeStat();
+
+	void InitStatData(int SerialNumber);
+
+private:
+	UPROPERTY(Transient, EditAnywhere, Category = "Stat", meta = (AllowPrivateAccess = true))
+	int32 CurrentHP;
+
+	UPROPERTY(Transient, EditAnywhere, Category = "Stat", meta = (AllowPrivateAccess = true))
+	int32 MaxHP;
+
+	UPROPERTY(Transient, EditAnywhere, Category = "Stat", meta = (AllowPrivateAccess = true))
+	int32 CurrentMP;
+
+	UPROPERTY(Transient, EditAnywhere, Category = "Stat", meta = (AllowPrivateAccess = true))
+	int32 MaxMP;
+
+	UPROPERTY(Transient, EditAnywhere, Category = "Stat", meta = (AllowPrivateAccess = true))
+	int32 AttackDamage;
+
+	UPROPERTY(Transient, EditAnywhere, Category = "Stat", meta = (AllowPrivateAccess = true))
+	int32 AbilityPower;
+
+	UPROPERTY(Transient, EditAnywhere, Category = "Stat", meta = (AllowPrivateAccess = true))
+	int32 Defense;
+
+	UPROPERTY(Transient, EditAnywhere, Category = "Stat", meta = (AllowPrivateAccess = true))
+	int32 MagicResistance;
+
+	UPROPERTY(Transient, EditAnywhere, Category = "Stat", meta = (AllowPrivateAccess = true))
+	float AttackSpeed;
+
+	UPROPERTY(Transient, EditAnywhere, Category = "Stat", meta = (AllowPrivateAccess = true))
+	int32 Range;
+
+	UPROPERTY(Transient, EditAnywhere, Category = "Stat", meta = (AllowPrivateAccess = true))
+	int32 CriticalRate;
+
+	UPROPERTY(Transient, EditAnywhere, Category = "Stat", meta = (AllowPrivateAccess = true))
+	float CriticalCoefficient;
+};
