9930e7bc405bfc18fc0c8c0910f98be710cf26cf
highstar97 <pgt0185@gmail.com>
2022-05-11 03:35:36 +0900

    GameMode Round 변수 추가

--- Source/HongtoChess/HCGameMode.cpp
+++ Source/HongtoChess/HCGameMode.cpp
@@ -1,2 +1,11 @@
 #include "HCGameMode.h"
 
+AHCGameMode::AHCGameMode()
+{
+	BigRound = 1;
+	SmallRound = 1;
+}
+
+// Round Play
+// Check Round Finish
+// Next Round
--- Source/HongtoChess/HCGameMode.h
+++ Source/HongtoChess/HCGameMode.h
@@ -8,4 +8,13 @@ class HONGTOCHESS_API AHCGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+	AHCGameMode();
+
+private:
+	UPROPERTY()
+	int32 BigRound;
+
+	UPROPERTY()
+	int32 SmallRound;
 };
