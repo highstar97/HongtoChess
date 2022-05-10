6ebd6413cf77e191d67b733c89400a89edf16923
highstar97 <pgt0185@gmail.com>
2022-05-11 00:47:04 +0900

    GameMode 이름 변경

---
+++ Source/HongtoChess/HCGameMode.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "HCGameMode.generated.h"
+
+UCLASS()
+class HONGTOCHESS_API AHCGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
