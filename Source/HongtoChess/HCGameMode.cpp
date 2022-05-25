3ea95c750f61b2c9e16a18e82d31e9ff21a80f20
highstar97 <pgt0185@gmail.com>
2022-05-25 22:30:24 +0900

    HCGameMode 생성

--- Source/HongtoChess/HCGameMode.cpp
+++ Source/HongtoChess/HCGameMode.cpp
@@ -1,9 +1,20 @@
 #include "HCGameMode.h"
+#include "HCPlayerController.h"
+#include "HCThirdPartySocketServerActor.h"
 
 AHCGameMode::AHCGameMode()
 {
+	PlayerControllerClass = AHCPlayerController::StaticClass();
+
+	//TPSocket = (AHCThirdPartySocketServer*)GetWorld()->SpawnActor(AHCThirdPartySocketServer::StaticClass());
+
 	BigRound = 1;
 	SmallRound = 1;
+}
+
+void AHCGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
 }
 
 // Round Play
--- Source/HongtoChess/HCGameMode.h
+++ Source/HongtoChess/HCGameMode.h
@@ -3,6 +3,8 @@
 #include "GameFramework/GameModeBase.h"
 #include "HCGameMode.generated.h"
 
+class AHCThirdPartySocketServerActor;
+
 UCLASS()
 class HONGTOCHESS_API AHCGameMode : public AGameModeBase
 {
@@ -11,10 +13,17 @@ class HONGTOCHESS_API AHCGameMode : public AGameModeBase
 public:
 	AHCGameMode();
 
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+	AHCThirdPartySocketServerActor* GetTPSocket() { return TPSocket; }
+
 private:
 	UPROPERTY()
 	int32 BigRound;
 
 	UPROPERTY()
 	int32 SmallRound;
+
+	UPROPERTY()
+	AHCThirdPartySocketServerActor* TPSocket;
 };
