40f4104a89966cd8fd8b0c5dde746a9c2d2ca892
highstar97 <pgt0185@gmail.com>
2022-05-27 02:10:05 +0900

    HCPlayerCharacter 생성

--- Source/HongtoChess/HCPlayerController.cpp
+++ Source/HongtoChess/HCPlayerController.cpp
@@ -1,6 +1,4 @@
 #include "HCPlayerController.h"
-#include "HCGameMode.h"
-#include "HCThirdPartySocketServerActor.h"
 
 void AHCPlayerController::PostInitializeComponents()
 {
@@ -15,13 +13,4 @@ void AHCPlayerController::OnPossess(APawn* aPawn)
 void AHCPlayerController::SetupInputComponent()
 {
 	Super::SetupInputComponent();
-	InputComponent->BindAction(TEXT("Test"), EInputEvent::IE_Pressed, this, &AHCPlayerController::Test);
-}
-
-void AHCPlayerController::Test()
-{
-	UE_LOG(LogTemp, Warning, TEXT("Shift S is Pressed"));
-
-	auto HCGameMode = Cast<AHCGameMode>(GetWorld()->GetAuthGameMode());
-	HCGameMode->GetTPSocket()->Test();
 }
